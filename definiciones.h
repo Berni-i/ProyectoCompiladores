3a2a6bbcc0567f040a2408b97974d8e7a1ac1f00
Berni-i <dbernardezf@gmail.com>
2022-03-13 00:12:29 +0100

    Pueba incluyendo más ficheros

---
+++ definiciones.h
@@ -0,0 +1,8 @@
+#define PACKAGE 500
+#define IMPORT 501
+#define FUNC 502
+#define CHAN 503
+#define VAR 504
+#define FOR 505
+#define RANGE 506
+#define GO 507
---
+++ tablaSimbolos.c
@@ -0,0 +1,12 @@
+#include "abb.h"
+#include "definiciones.h"
+
+abb tablaSimbolos;
+
+void inicializarTabla(){
+    //función para iniciar la tabla con las palabras reservadas
+
+    //Se crea el árbol binario
+    crear(&tablaSimbolos);
+
+}
